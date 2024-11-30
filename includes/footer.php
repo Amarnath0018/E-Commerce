aa7f22bcded387ef95297c5a3c4384af7250c6c7
Amarnath Sekar <amarnathsekar13102000@gamil.com>
2024-11-30 12:32:16 +0530

    basic structure

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Website Header</title>
+</head>
+<body>
+    <footer>
+        <p>&copy; 2024 My PHP Project</p>
+    </footer>
+</body>
+</html>
